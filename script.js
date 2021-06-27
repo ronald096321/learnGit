bee5532a122afbb19647018ac9ab821df8b7327e
ronald096321 <vinaypk07@gmail.com>
2021-06-27 15:32:37 +0530

    Modified commit test

--- script.js
+++ script.js
@@ -71,7 +71,8 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// Trying new commit
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
